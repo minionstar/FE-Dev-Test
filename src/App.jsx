6ab40321131886ecb7b5dd0aa4eb4e0fda20a283
minionstar <louislee214@gmail.com>
2024-05-09 10:17:11 -0400

    update/updated app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { fetchPosts, fetchUsers } from './api/api';
 import PostList from './components/PostList';
 import FilterComponent from './components/FilterComponent';
 import SummaryComponent from './components/SummaryComponent';
-import Pagination from './components/Pagination'; // Import the Pagination component
 import './index.css';
 
 function App() {
@@ -11,8 +10,6 @@ function App() {
     const [posts, setPosts] = useState([]);
     const [users, setUsers] = useState([]);
     const [filteredPosts, setFilteredPosts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(10); // You can adjust the number of posts per page
 
     useEffect(() => {
         // Asynchronously fetch posts and users from the API when the component mounts
@@ -42,14 +39,6 @@ function App() {
         setFilteredPosts(filtered);
     };
 
-    // Get current posts
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
-
-    // Change page
-    const paginate = pageNumber => setCurrentPage(pageNumber);
-
     return (
         <div className="p-10 flex flex-row justify-center w-full">
             <div className='max-w-7xl flex flex-col'>
@@ -59,13 +48,7 @@ function App() {
                     <SummaryComponent posts={filteredPosts} users={users} />
                 </div>
                 <hr className='my-5' />
-                <PostList posts={currentPosts} />
-                <Pagination 
-                    currentPage={currentPage} 
-                    totalPosts={filteredPosts.length} 
-                    postsPerPage={postsPerPage} 
-                    paginate={paginate} 
-                />
+                <PostList posts={filteredPosts} />
             </div>
         </div>
     );
